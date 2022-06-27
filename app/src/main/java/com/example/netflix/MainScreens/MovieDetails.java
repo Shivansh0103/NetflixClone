2ae011dc7e88cc788b33eb84e2abcfc76c20541d
Shivansh <singh.shivansh0103@gmail.com>
2022-06-27 11:34:25 +0530

    Assignment done

--- app/src/main/java/com/example/netflix/MainScreens/MovieDetails.java
+++ app/src/main/java/com/example/netflix/MainScreens/MovieDetails.java
@@ -21,20 +21,29 @@ import com.example.netflix.Activities.PaymentGateway;
 import com.example.netflix.Adapters.CommentAdapter;
 import com.example.netflix.Model.Comment;
 import com.example.netflix.R;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.FirebaseNetworkException;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.firestore.SetOptions;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class MovieDetails extends AppCompatActivity {
     ImageView movieImage;
@@ -44,9 +53,10 @@ public class MovieDetails extends AppCompatActivity {
     DocumentReference userReference;
     FirebaseFirestore firebaseFirestore;
     Button play,add;
-    String userID,name,image,fileUrl,movieId,userName,userComment;
+    String userID,name,image,fileUrl,movieId,userName,userComment,mName;
     RecyclerView commentBox;
     CommentAdapter commentAdapter;
+    Map<String,Object> m;
     List<Comment> commentList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -93,10 +103,11 @@ public class MovieDetails extends AppCompatActivity {
             public void onClick(View view) {
                 Map<String, Object> user=new HashMap<String,Object>();
                 String commentContent=commentboxedittext.getText().toString();
-                user.put("Comment",commentContent);
+                user.put(name,commentContent);
                 userReference.set(user, SetOptions.merge()).addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
                     public void onSuccess(Void unused) {
+                        iniRvComment();
                         Toast.makeText(MovieDetails.this, "Comment Added", Toast.LENGTH_SHORT).show();
                     }
                 }).addOnFailureListener(new OnFailureListener() {
@@ -124,16 +135,17 @@ public class MovieDetails extends AppCompatActivity {
 
     private void iniRvComment() {
         commentBox.setLayoutManager(new LinearLayoutManager(this));
-        userID=firebaseAuth.getCurrentUser().getUid();
-        userReference=firebaseFirestore.collection("Users ").document(userID);
         commentList=new ArrayList<>();
         if(firebaseAuth.getCurrentUser()!=null)
         {
             userReference.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
                 @Override
                 public void onSuccess(DocumentSnapshot documentSnapshot) {
+                    userComment=documentSnapshot.getString(name);
+                    if(documentSnapshot.contains(name))
                     userName=documentSnapshot.getString("First Name")+" "+documentSnapshot.getString("Last Name");
-                    userComment=documentSnapshot.getString("Comment");
+                    else
+                        userName="";
                     Comment comment=new Comment(userComment,userID,userName);
                     commentList.add(comment);
                     commentAdapter=new CommentAdapter(getApplicationContext(),commentList);
